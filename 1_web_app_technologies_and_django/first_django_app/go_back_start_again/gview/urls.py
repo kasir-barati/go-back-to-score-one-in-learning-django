1a0312aa34353f6786ca9b97da7b75f0cc04d469
Kasir <kasir.barati@gmail.com>
2022-07-04 03:22:17 +0430

    rename

---
+++ 1_web_app_technologies_and_django/first_django_app/go_back_start_again/gview/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+from .views import ListApple
+from .views import CsrfProtectedForm
+
+
+app_name='gview'
+
+
+urlpatterns = [
+    # Wired an index or anything view into the URLconf
+    path(
+        '', 
+        TemplateView.as_view(template_name="gview/index.html"), 
+        name="gview-index"
+    ),
+    path('apples', ListApple.as_view(), name='apple-list'),
+    path('guess', CsrfProtectedForm.as_view(), name='guess-me-protected')
+]
